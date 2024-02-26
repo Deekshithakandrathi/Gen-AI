d0ce29eee6ed96563d5a7c81e7e6db4e4e9f2070
Deekshithakandrathi <161105903+Deekshithakandrathi@users.noreply.github.com>
2024-02-26 21:20:59 +0530

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -5,3 +5,95 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 
 openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+#     print(str(response.choices[0].message))
+
+  messages =  [  
+{'role':'system', 'content':'You are an assistant that speaks like Shakespeare.'},    
+{'role':'user', 'content':'tell me a joke'},   
+{'role':'assistant', 'content':'Why did the chicken cross the road'},   
+{'role':'user', 'content':'I don\'t know'}  ]
+    return response.choices[0].message["content"]
+
+response = get_completion_from_messages(messages, temperature=1)
+print(response)
+
+messages =  [  
+{'role':'system', 'content':'You are friendly chatbot.'},    
+{'role':'user', 'content':'Hi, my name is Isa'}  ]
+response = get_completion_from_messages(messages, temperature=1)
+print(response)
+
+messages =  [  
+{'role':'system', 'content':'You are friendly chatbot.'},    
+{'role':'user', 'content':'Yes,  can you remind me, What is my name?'}  ]
+response = get_completion_from_messages(messages, temperature=1)
+print(response)
+
+messages =  [  
+{'role':'system', 'content':'You are friendly chatbot.'},
+{'role':'user', 'content':'Hi, my name is Isa'},
+{'role':'assistant', 'content': "Hi Isa! It's nice to meet you. \
+Is there anything I can help you with today?"},
+{'role':'user', 'content':'Yes, you can remind me, What is my name?'}  ]
+response = get_completion_from_messages(messages, temperature=1)
+print(response)
+
+def collect_messages(_):
+    prompt = inp.value_input
+    inp.value = ''
+    context.append({'role': 'user', 'content': f"{prompt}"})
+    response = get_completion_from_messages(context)
+    context.append({'role': 'assistant', 'content': f"{response}"})
+    panels.append(
+        pn.Row('User:', pn.pane.Markdown(prompt, width=600)))
+    panels.append(
+        pn.Row('Assistant:', pn.pane.Markdown(response, width=600, style={'background-color': '#F6F6F6'})))
+
+    return pn.Column(*panels)
+
+import panel as pn  # GUI
+pn.extension()
+
+panels = [] # collect display 
+
+context = [{'role': 'system', 'content': """
+You are FashionBot, here to assist you with fashion-related inquiries. \
+You can ask me about clothing trends, styling tips, outfit ideas, fashion brands, and more. \
+Feel free to inquire about any specific fashion topic you're interested in. Just let me know what you need and I'll do my best to help you out!\
+"""}]  # accumulate messages
+
+inp = pn.widgets.TextInput(value="Hi", placeholder='Enter text here…')
+button_conversation = pn.widgets.Button(name="Chat!")
+
+interactive_conversation = pn.bind(collect_messages, button_conversation)
+
+dashboard = pn.Column(
+    inp,
+    pn.Row(button_conversation),
+    pn.panel(interactive_conversation, loading_indicator=True, height=300),
+)
+
+dashboardmessages =  context.copy()
+messages.append({
+    'role': 'system',
+    'content': 'create a json summary of the previous food order. Itemize the price for each item. The fields should be 1) pizza, include size 2) list of toppings 3) list of drinks, include size 4) list of sides include size 5) total price'
+})
+
+response = get_completion_from_messages(messages, temperature=0)
+print(response)
